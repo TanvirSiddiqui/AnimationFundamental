bf344e0eb779cf1a584bfb85ae04d97e8e9ec054
Tanvir Siddiqui <babatanvir@hotmail.com>
2024-09-10 02:20:02 +0600

    CameraController script added.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f; // Adjust smoothSpeed to control the camera's following speed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
